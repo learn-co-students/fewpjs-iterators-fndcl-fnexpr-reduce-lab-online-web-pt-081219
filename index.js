9782679514a49a22fef49291eec1641e485b6c1d
Jenna Ransom <jenna.ransom@gmail.com>
2020-03-29 20:30:53 -0600

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+let totalBatteries = batteryBatches.reduce(function(total, num) {
+  return total += num
+});
+
+// or 
+// let totalBatteries = batteryBatches.reduce(addBatteries);
+
+// function addBatteries(total, amount) {
+//   return total + amount
+// }
+
